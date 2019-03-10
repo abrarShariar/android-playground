52ca2bcedd2cd21be778fafbaca6ee8d9a740961
Abrar Shariar <shariarabrar@gmail.com>
2019-03-10 23:19:21 +0900

    Text passing

--- app/src/main/java/com/example/androidtest/MainActivity.java
+++ app/src/main/java/com/example/androidtest/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.androidtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public void changeText (View view) {
+        TextView helloText = (TextView) findViewById(R.id.helloTitleTextView);
+        Log.i("From change Text", "Changing Text");
+        helloText.setText("YO!");
+
+        EditText nameTextBox = (EditText) findViewById(R.id.textInputEditText);
+        String name = nameTextBox.getText().toString();
+
+        helloText.setText(name);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
